34d8b3668b81317b28974669475d76d2c46e0f9e
dimitrioschr <dimitrioschr@gmail.com>
2015-07-17 16:53:30 +0300

    housekeeping

--- test1.py
+++ test1.py
@@ -4,4 +4,13 @@ import pandas as pd
 import numpy as np
 import xlrd
 import sqlite3
+import os
 
+
+pfdbconn = sqlite3.connect(os.sep.join([os.getcwd(), 'databases', 'pfdb.db']))
+pfdbconn.commit()
+pfdbconn.close()
+
+vsldbconn = sqlite3.connect(os.sep.join([os.getcwd(), 'databases', 'vsldb.db']))
+vsldbconn.commit()
+vsldbconn.close()
