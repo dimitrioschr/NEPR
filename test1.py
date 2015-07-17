195cae5fa8d79159648ab694d140e5a6b163c52f
dimitrioschr <dimitrioschr@gmail.com>
2015-07-17 17:12:21 +0300

    housekeeping

--- test1.py
+++ test1.py
@@ -14,3 +14,15 @@ pfdbconn.close()
 vsldbconn = sqlite3.connect(os.sep.join([os.getcwd(), 'databases', 'vsldb.db']))
 vsldbconn.commit()
 vsldbconn.close()
+
+filename = os.sep.join([os.getcwd(),
+                        'vslfolder',
+                        'NEPR PHOENIX RISING',
+                        'NEPR PHOENIX_RISING (1B2015) Chiba-Long Beach.xlsx'])
+print(filename)
+book = xlrd.open_workbook(filename)
+sheet = book.sheet_by_index(0)
+print(sheet.col(1)[2])
+
+neprfolders = os.listdir(os.sep.join([os.getcwd(), 'vslfolder']))
+print(neprfolders)
