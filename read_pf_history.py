41f814e159b0aac2fcf9b7e3f26562cd1dc002bc
dimitrioschr <dimitrioschr@gmail.com>
2015-07-20 17:52:47 +0300

    housekeeping

--- read_pf_history.py
+++ read_pf_history.py
@@ -11,5 +11,16 @@ def read_pf_history():
     data = pd.read_html(history_file, header=0)[0]
     return data
 
-ddd = read_pf_history()
-print(ddd[pd.isnull(ddd['Wind & swell wave height(meters)'])]['Speed(knots)'])
+# ddd = read_pf_history()
+# print(ddd[pd.isnull(ddd['Wind & swell wave height(meters)'])]['Speed(knots)'])
+
+def read_last_report():
+    report_file = os.sep.join([os.getcwd(),
+                               'pffolder',
+                               'data_history'])
+    data = pd.read_html(report_file, header=0)[0]
+    return data
+
+ddd = read_last_report()
+ddd = ddd[[0, 2, 3, 4, 6, 7, 16, 17, 18, 19, 20, 21, 23, 24, 26, 27, 29]]
+print(ddd.tail())
