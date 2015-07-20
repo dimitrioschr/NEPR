6d2be7b6b521300d8251b1ecc8359551d7888649
dimitrioschr <dimitrioschr@gmail.com>
2015-07-20 18:36:45 +0300

    housekeeping

--- read_pf_history.py
+++ read_pf_history.py
@@ -22,5 +22,7 @@ def read_last_report():
     return data
 
 ddd = read_last_report()
-ddd = ddd[[0, 2, 3, 4, 6, 7, 16, 17, 18, 19, 20, 21, 23, 24, 26, 27, 29]]
-print(ddd.tail())
+ddd = ddd[[0, 2, 3, 4, 6, 7, 16, 17, 18, 19, 20, 21, 23, 24, 26, 29]]
+print(ddd.columns)
+print(ddd.tail(1))
+print(type(ddd.iloc[-1, 1]))
