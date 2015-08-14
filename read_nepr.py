7f7376ba8bfc666e40598ce3d72994fd0042bbaf
dimitrioschr <dimitrioschr@gmail.com>
2015-08-14 15:27:36 +0300

    housekeeping

--- read_nepr.py
+++ read_nepr.py
@@ -1,6 +1,7 @@
 
 import xlrd
 import pandas as pd
+import numpy as  np
 from pandas import DataFrame
 
 
@@ -27,6 +28,8 @@ def df_from_sheet(sheet, basecol_number, columns_to_read, datetime_columns, trim
             data.append(read_column(c, start_row, n_act_rows))
     return DataFrame(data).T
 # get a Dataframe from a specified sheet
+make_str_datetime = np.vectorize(lambda a, b: '.'.join([str(e) for e in a[:3]]) + ' ' + ':'.join([str(e) for e in b[3:5]]))
+# make a string datetime to pass to pandas.to_datetime, vectorised
 
 
 book = xlrd.open_workbook('test.xlsx')
@@ -41,6 +44,8 @@ start_row = 8
 # data start from row 8
 
 df1 = df_from_sheet(sheet, basecol_number, columns_to_read, datetime_columns, trim_columns, start_row)
+df1 = df1.set_index(pd.to_datetime(make_str_datetime(df1[0], df1[1])))
+df1.drop(df1.columns[[0, 1]], inplace=True, axis=1)
 print(df1)
 
 
@@ -54,6 +59,8 @@ start_row = 2
 # data start from row 2
 
 df2 = df_from_sheet(sheet, basecol_number, columns_to_read, datetime_columns, trim_columns, start_row)
+df2 = df2.set_index(pd.to_datetime(make_str_datetime(df2[0], df2[1])))
+df2.drop(df2.columns[[0, 1]], inplace=True, axis=1)
 print(df2)
 
 
@@ -68,6 +75,8 @@ start_row = 2
 
 
 df3 = df_from_sheet(sheet, basecol_number, columns_to_read, datetime_columns, trim_columns, start_row)
+df3 = df3.set_index(pd.to_datetime(make_str_datetime(df3[0], df3[1])))
+df3.drop(df3.columns[[0, 1]], inplace=True, axis=1)
 print(df3)
 
 # process sheet 'Environmental'
@@ -81,7 +90,11 @@ start_row = 5
 
 
 df4 = df_from_sheet(sheet, basecol_number, columns_to_read, datetime_columns, trim_columns, start_row)
+df4 = df4.set_index(pd.to_datetime(make_str_datetime(df4[0], df4[1])))
+df4.drop(df4.columns[[0, 1]], inplace=True, axis=1)
 print(df4)
 
 
-res = df1.merge(df2, on=[0, 1]).merge(df3, on=[0, 1]).merge(df4, on=[0, 1])
+print('\n' * 11)
+res = df1.merge(df2, left_index=True, right_index=True).merge(df3, left_index=True, right_index=True).merge(df4, left_index=True, right_index=True)
+print(res)
